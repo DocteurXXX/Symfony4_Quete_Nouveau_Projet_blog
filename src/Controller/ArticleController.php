f23b35e05ec80172afb87eb2ec39bd3e58a4c6ee
BEAUHAIRE Julien <Julien_BEAUHAIRE1@MacBook-Pro-de-BEAUHAIRE-2.local>
2019-05-22 13:48:37 +0200

    Symfony : Entity Type

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use App\Entity\Tag;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +18,12 @@ class ArticleType extends AbstractType
             ->add('title')
             ->add('content')
             ->add('category', null, ['choice_label' => 'name'])
-            /*->add('tags')*/
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'name',
+                'expanded' => 'true',
+                'multiple' => 'true',
+            ]);
         ;
     }
 
